ea1634e23bf2378416ebe5cf9994c3e6fef1314f
dewathiwat <Athiwat_Jansomwong@cmu.ac.th>
2021-09-15 23:19:44 +0700

    index page update

---
+++ react-app/src/features/Indexpage/features/HomePage/index.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Axios from "axios";
+import NowPlaying from "./nowplaying";
+import { HomeWrapper } from "./style";
+
+export default function HomePage() {
+  const [Action, setAction] = useState([]);
+  const [Comedy, setComedy] = useState([]);
+  const [Drama, setDrama] = useState([]);
+
+  useEffect(() => {
+    getAllMovies();
+  }, []);
+  const getAllMovies = async () => {
+    const allMovies = await Axios.all([
+      Axios.get(
+        `https://api.aniapi.com/v1/anime?genres=Action`
+      ),
+      Axios.get(
+        `https://api.aniapi.com/v1/anime?genres=Comedy`
+      ),
+      Axios.get(
+        `https://api.aniapi.com/v1/anime?genres=Drama`
+      )
+    ]);
+    setAction(allMovies[0].data.data.documents);
+    setComedy(allMovies[1].data.data.documents);
+    setDrama(allMovies[2].data.data.documents);
+  };
+
+  return (
+    <>
+    <HomeWrapper>
+      <NowPlaying movie={Action} name={"Action"} />
+      <NowPlaying movie={Comedy} name={"Comedy"}/>
+      <NowPlaying movie={Drama} name={"Drama"}/>
+    </HomeWrapper>
+    </>
+  );
+}
---
+++ react-app/src/features/Indexpage/features/HomePage/nowplaying/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Container, Card, Row, Col } from "react-bootstrap";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function NowPlaying({ movie,name }) {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  };
+  return (
+    <Container>
+      <div className="clearfix mt-5 mb-2">
+        <h4 className="float-left">{name}</h4>
+      </div>
+      <Slider {...settings}>
+        {movie.map(function(movie) {
+          return (
+            <React.Fragment>
+                <Col>
+                  <Card>
+                    <Card.Img
+                      variant="top"
+                      src={movie.cover_image}
+                    />
+                    <Card.Body>
+                      <span>{movie.titles.en}</span>
+                    </Card.Body>
+                  </Card>
+                </Col>
+            </React.Fragment>
+          );
+        })}
+      </Slider>
+    </Container>
+  );
+}
