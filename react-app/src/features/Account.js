814e3e4193a5d282125fc700c66201c2c4dd93cd
Thamonwan <thamonwan_as@cmu.ac.th>
2021-09-16 11:04:06 +0700

    Edit fragment(<>)

--- react-app/src/features/Account.js
+++ react-app/src/features/Account.js
@@ -7,41 +7,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import styled from 'styled-components';
 
 function Account() {
-  <>
-    return (
-    <div className=''>
-      <h1>Account</h1>
-      <hr></hr>
 
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>User ID</Form.Label>
-          <Form.Control type="id" placeholder="ID" />
-        </Form.Group>
+  return (
+    <>
+      <div className=''>
+        <h1>Account</h1>
+        <hr></hr>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>User name</Form.Label>
-          <Form.Control type="text" placeholder="name" />
-        </Form.Group>
-      </Form>
-      <hr></hr>
-    </div>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>User ID</Form.Label>
+            <Form.Control type="id" placeholder="ID" />
+          </Form.Group>
 
-    <div className=''>
-      <CloseButton />
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </div>
-    );
-  </>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>User name</Form.Label>
+            <Form.Control type="text" placeholder="name" />
+          </Form.Group>
+        </Form>
+        <hr></hr>
+      </div>
+
+      <div className=''>
+        <CloseButton />
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
+  );
+
 }
 
 export default Account;
