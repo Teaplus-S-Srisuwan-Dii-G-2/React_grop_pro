1a7953faa90ffe3343ebd1789e6f3f7bdc91feae
dewathiwat <Athiwat_Jansomwong@cmu.ac.th>
2021-09-20 23:07:52 +0700

    เพิ่ม Link navbar

--- react-app/src/features/Navbar.js
+++ react-app/src/features/Navbar.js
@@ -1,19 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Navbar from 'react-bootstrap/Navbar';
+import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav'
+import  Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
 
 
 function navbar({className}) {
     return (
       <Navbar bg="dark" variant="dark">
-      <Container>
-        <Link to="/"><Navbar.Brand href="#home">
-        AnimeList
-        </Navbar.Brand></Link>
-      </Container>
-    </Navbar>
+    <Container>
+    <Navbar.Brand href="#home">AnimeList</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">movie de</Nav.Link>
+      <Nav.Link href="#features">accoud</Nav.Link>
+    </Nav>
+    <Form className="d-flex">
+      <FormControl
+        type="search"
+        placeholder="Search"
+        className="mr-2"
+        aria-label="Search"
+      />
+      <Button variant="outline-light">Search</Button>
+    </Form>
+    </Container>
+  </Navbar>
     );
   }
   Navbar.propTypes = {
