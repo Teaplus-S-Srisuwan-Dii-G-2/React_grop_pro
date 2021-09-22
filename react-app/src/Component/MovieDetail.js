62af3d11dbef7e5ed1c08247adbbb3466c35e4c2
ThanakornJ <thanakorn_jansa@elearning.cmu.ac.th>
2021-09-22 12:13:34 +0700

    แกไข ปุ่ม like

--- react-app/src/Component/MovieDetail.js
+++ react-app/src/Component/MovieDetail.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchProducts } from './anime/actions'
 
+
 function AnimeDetail() {
   //   const [topAnime, SetTopAnime] = useState([]);
   const animes = useSelector((state) => state.animes);
@@ -23,7 +24,8 @@ function AnimeDetail() {
   }, []);
 
   return (
-    <main>
+
+    <main>  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
       <center className="animed-topic">
         <p className="animed-topic-title">Anime Detail</p>
       </center>
@@ -56,21 +58,27 @@ function AnimeDetail() {
                   <li></li>
                 </ul>
               </div>
+
+            </div>
+            <div className="animed-button">
+              <div class="icon">
+                <img src="https://pnglux.com/wp-content/uploads/2021/04/1617443306_Like-PNG-High-Quality-Image.png" />
+
+                {/* <img classs="icon" src="https://pnglux.com/wp-content/uploads/2021/04/1617443306_Like-PNG-High-Quality-Image.png"/> */}
+              </div>
+
+              <button class="button">
+                <Link to={`/animeselect/${anime.mal_id}`}>
+
+                  <a href={anime.trailer_url}>
+                    <span>Find out more</span>
+                  </a>
+
+                </Link>   </button>
+
             </div>
 
-<button class="button-like">
-              <a href={anime.trailer_url}>
-                <span>LIke</span>
-              </a>
-            </button>
-          
-            <Link to={`/animeselect/${anime.mal_id}`}>
-              <button class="button">
-                <a href={anime.trailer_url}>
-                  <span>Find out more</span>
-                </a>
-              </button>
-            </Link>  
+
           </div>
         ))}
       </div>
